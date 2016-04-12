694268200018fc03343043ab2e6dc2f39cb94610
Alexey Romanov <alexey.romanov@huawei.com>
2016-04-12 15:53:29 +0300

    Add superCall macro

---
+++ src/main/scala/com/github/alexeyr/scaladry/Super.scala
@@ -0,0 +1,39 @@
+package com.github.alexeyr.scaladry
+
+import macrocompat.bundle
+
+import scala.reflect.macros.whitebox
+
+object Super {
+  /** When used inside a method definition, resolves to a super call of the same method with the same parameters.
+    * Note that this includes implicit parameters, even if they are shadowed in this scope.
+    * To specify which `super` to call, see [[superCallOf]].
+    */
+  def superCall: Any = macro SuperImpl.superCallOf[Nothing]
+
+  /** Same as [[superCall]], but resolves to `super[T]` where [[T]] is the type parameter.
+    */
+  def superCallOf[T]: Any = macro SuperImpl.superCallOf[T]
+}
+
+@bundle
+private class SuperImpl(val c: whitebox.Context) {
+  import c.universe._
+
+  def superCallOf[T: WeakTypeTag]: Tree = {
+    c.internal.enclosingOwner match {
+      case method: MethodSymbol =>
+        if (method.overrides.isEmpty)
+          c.abort(c.enclosingPosition, s"method ${method.name} overrides nothing, superCall makes no sense")
+        val args = method.paramLists.map(_.map(sym => c.Expr(q"$sym")))
+        val typeOfT = weakTypeOf[T]
+        val typeParams = method.typeParams.map(_.asType.name)
+        if (typeOfT =:= typeOf[Nothing])
+          q"super.${method.name}[..$typeParams](...$args)"
+        else
+          q"super[${typeOfT.typeSymbol.asType.name}].${method.name}[..$typeParams](...$args)"
+      case _ =>
+        c.abort(c.enclosingPosition, "superCall must be contained in a method")
+    }
+  }
+}

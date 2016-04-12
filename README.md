8e7ef62d8b1a3e8b6b2a99a895ed3dca4ce08858
Alexey Romanov <alexey.v.romanov@gmail.com>
2016-04-12 15:53:29 +0300

    Add superCall macro

--- README.md
+++ README.md
@@ -2,3 +2,48 @@
 Scala macros which help you not to repeat yourself
 
 [![Build Status](https://travis-ci.org/alexeyr/scala-dry.svg?branch=master)](https://travis-ci.org/alexeyr/scala-dry)
+
+## `super` calls
+Most often `super` calls use the same method name and arguments, simply doing something before or after the call
+or changing the result in some way, something like
+    
+    class HelloAndroid extends Activity {
+      override def onCreate(savedInstanceState: Bundle): Unit = {
+        super.onCreate(savedInstanceState)
+        ...
+      }
+    }
+    
+Using `com.github.alexeyr.scaladry.Super.superCall`, you can write the above as
+ 
+    class HelloAndroid extends Activity {
+      override def onCreate(savedInstanceState: Bundle): Unit = {
+        superCall
+        ...
+      }
+    }
+
+instead. Obviously in this case the win isn't large, because `onCreate` signature is quite unlikely to change,
+but for methods in your own classes or third-party libraries, whenever method signature changes, the `super` calls need
+to be changed as well. If you forget to do so, you can unintentionally call an overloaded version, e.g.
+
+    // Was:
+    class A {
+      def foo(): Int = ...
+    }
+    
+    class B extends A {
+      override def foo() = super.foo() + 1
+    }
+    
+    // Became:
+    class A {
+      def foo(x: Int): Int = ...
+      def foo(): Int = foo(0)
+    }
+    
+    class B extends A {
+      override def foo(x: Int) = super.foo() + 1 // should have been super.foo(x) + 1
+    }
+
+If there are several overridden methods and you need to specify which one to call, use `superCallOf[A]` instead.
---
+++ src/test/scala/com/github/alexeyr/scaladry/SuperTests.scala
@@ -0,0 +1,117 @@
+package com.github.alexeyr.scaladry
+
+import utest._
+import Super._
+
+object SuperTests extends TestSuite {
+  val tests = this {
+    'simplestCase {
+      class A {
+        def foo(x: String) = x.length
+      }
+
+      'works {
+        class B extends A {
+          override def foo(x: String) = superCall + 1
+        }
+
+        (new B).foo("") ==> 1
+      }
+
+      'mustOverrideSomething {
+        compileError("class C extends A { def bar(x: Int) = superCall + 1 }")
+
+        compileError("class D extends A { def foo(x: Int): Int = superCall + 1 }")
+      }
+    }
+
+    'doubleInheritance {
+      trait A {
+        def foo(x: String) = x.length
+      }
+
+      trait B {
+        def foo(x: String) = x.length + 1
+      }
+
+      class C extends A with B {
+        override def foo(x: String) = superCallOf[A]
+      }
+
+      class D extends A with B {
+        override def foo(x: String) = superCallOf[B]
+      }
+
+      (new C).foo("") ==> 0
+
+      (new D).foo("") ==> 1
+    }
+
+    'overloadedMethods {
+      class A {
+        def foo: Int = 0
+        def foo(x: Int): Int = 1
+        def foo(x: Double): Int = 2
+      }
+
+      class B extends A {
+        override def foo: Int = superCall
+        override def foo(x: Int): Int = superCall
+        override def foo(x: Double): Int = superCall
+      }
+
+      val b = new B
+      b.foo ==> 0
+      b.foo(0) ==> 1
+      b.foo(0.0) ==> 2
+    }
+
+    // FIXME fails to compile:
+    //  [error]  found   : y.type (with underlying type A)
+    //  [error]  required: A
+    //  [error]         override def foo[A](x: Int, y: A) = superCall // super.foo[A](x, y) works!
+    //  [error]                                             ^
+    //    'typeParams {
+    //      class Foo[A] {
+    //        def foo[B](x: A, y: B) = y
+    //      }
+    //
+    //      class Bar[A] extends Foo[A] {
+    //        override def foo[B](x: A, y: B) = superCall
+    //      }
+    //
+    //      class Baz extends Foo[Int] {
+    //        override def foo[A](x: Int, y: A) = superCall // super.foo[A](x, y) works!
+    //      }
+    //
+    //      (new Bar[Int]).foo(0, 0.0) ==> 0.0
+    //      (new Baz).foo(0, 0.0) ==> 0.0
+    //    }
+
+    'multipleParamLists {
+      class A {
+        def foo(x: Int)(y: String)(implicit z: Int) = x.toString + y + z.toString
+      }
+
+      class B extends A {
+        override def foo(x: Int)(y: String)(implicit z: Int) = s"B: $superCall"
+      }
+
+      implicit val i: Int = 1
+      (new B).foo(0)("a") ==> "B: 0a1"
+
+      'shadowedImplicits {
+        class C extends A {
+          override def foo(x: Int)(y: String)(implicit z: Int) = {
+            // shadows z from parameter list
+            implicit val z: Int = 2
+            s"C: $superCall"
+          }
+        }
+
+        implicit val i: Int = 1
+        (new C).foo(0)("a") ==> "C: 0a1" // not C: 0a2!
+      }
+    }
+  }
+}
